aaed5e39a2585a415812370c9ba5307798597e22
Taiyo L Baniecki <tlb416@nyu.edu>
2020-10-11 19:14:50 +0900

    Recommit the project

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private Transform target;
+    public float smoothTime = 0.3f;
+
+    Vector3 velocity = Vector3.zero;
+
+
+    public float maxTime = 2f;
+    public float leftEnd = 2;
+    public float rightEnd = 80;
+    private bool stopCamera = false;
+    private void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+    public void FixedUpdate()
+    {
+        if (stopCamera) return;
+        Vector3 targetPosition;
+        targetPosition = target.TransformPoint(new Vector3(0, 2, -10f));
+        if (target.transform.position.x < leftEnd)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(leftEnd, targetPosition.y, targetPosition.z), ref velocity, smoothTime);
+        } else if (target.transform.position.x > rightEnd) {
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(rightEnd, targetPosition.y, targetPosition.z), ref velocity, smoothTime);
+        } else
+        {
+            //smoothly move the camera towards the target position
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+
+        
+    }
+
+    public void stopCameraMovement()
+    {
+        stopCamera = true;
+    }
+}
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    private Queue<string> sentences;
+    public float interval = 0.05f;
+
+    private GameObject parent;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue, GameObject p)
+    {
+        parent = p;
+        nameText.text = dialogue.name;
+
+
+        //保存用queueの中身を消去
+
+        sentences.Clear();
+
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        
+
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+
+        if (sentences.Count == 0)
+        {
+           EndDialogue();
+           return;
+        }
+
+        string sentence = sentences.Dequeue();
+
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+        
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        //1フレームに1文字出るようにする
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(interval);
+        }
+
+    }
+    void EndDialogue()
+    {
+        sentences.Clear();
+        if(parent != null)parent.GetComponent<TalkTo>().setTalking(false);
+    }
+}
